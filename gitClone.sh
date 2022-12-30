3dc63d046a85709a39dbea60e8abcfa76c64dd5f
s33ding <robertomdiniz@protonmail.com>
2022-12-29 22:01:20 -0300

    saving

---
+++ gitClone.sh
@@ -0,0 +1,3 @@
+read -p 'REPOSITORY: ' repository
+echo git clone git@github.com:$github_account/$repository.git
+git clone git@github.com:$github_account/$repository.git
---
+++ mysql-cli.sh
@@ -0,0 +1,1 @@
+mysql -h  $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p
