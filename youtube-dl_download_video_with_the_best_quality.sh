d0c7f73803d714ebdaed0f4dba6a3cd18b06db83
s33ding <robertomdiniz@protonmail.com>
2023-04-04 09:01:44 -0300

    saving

--- youtube-dl_download_video_with_the_best_quality.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ask the user for the video URL
-read -p "Enter the YouTube video URL: " video_url
-
-# Download the video in the best available quality up to 1080p
-youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best' $video_url
-
